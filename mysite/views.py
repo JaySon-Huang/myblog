fbe8a36537e7c6dcbf4347a5c0404d920d46e7bd
JaySon <tshent@qq.com>
2014-08-21 02:28:46 +0800

    1.不

--- mysite/views.py
+++ mysite/views.py
@@ -7,10 +7,15 @@ from django.shortcuts import render_to_response
 from blog.models import BlogPost
 from profile import profile_list
 
+from markdown2 import markdown
+
 def index(request):
 	global profile_list
 
 	blog_list = BlogPost.objects.all()
+
+	for blog in blog_list:
+		blog.body = markdown(blog.body[:240])
 
 	return render_to_response(
 		'index.html',
