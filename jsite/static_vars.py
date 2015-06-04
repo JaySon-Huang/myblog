ac2c9f450c83a7d44617c4f91b36ccc515257b06
JaySon-Huang <jayson.hjs@gmail.com>
2015-06-05 00:47:34 +0800

    deploy @ SAE

--- jsite/static_vars.py
+++ jsite/static_vars.py
@@ -2,24 +2,24 @@
 #encoding=utf-8
 
 profile_list = (
-    {'url': '/', 'title': '博客'},
+    {'url': '/', 'title': u'博客'},
     {
         'url': '/lab',
         'title': 'Lab',
         'drop_downs': (
-            {'url': '/lab/grass', 'title': '机械草丛'},
+            {'url': '/lab/grass', 'title': u'机械草丛'},
         )
     },
-    {'url': 'http://www.zhihu.com/people/jayson-24', 'title': '知乎'},
-    {'url': 'https://github.com/JaySon-Huang', 'title': 'GitHub'},
-    {'url': 'http://www.wooyun.org/whitehats/JaySon', 'title': '乌云'},
+    {'url': 'http://www.zhihu.com/people/jayson-24', 'title': u'知乎'},
+    {'url': 'https://github.com/JaySon-Huang', 'title': u'GitHub'},
+    {'url': 'http://www.wooyun.org/whitehats/JaySon', 'title': u'乌云'},
 )
 
 # SEO 优化
 from meta.views import Meta
 meta = Meta(
-    title="JaySon的小黑屋",
-    description="JaySon的编程笔记, 生活碎碎念",
+    title=u"JaySon的小黑屋",
+    description=u"JaySon的编程笔记, 生活碎碎念",
     keywords=['Information Security', 'JaySon', 'blog', ],
     extra_props={
         'viewport': 'width=device-width, initial-scale=1.0, minimum-scale=1.0'
